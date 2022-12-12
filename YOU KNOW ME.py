35129d519079f071715af0ad9d9fe78021f5fbcd
SenkUU909 <118453009+SenkUU909@users.noreply.github.com>
2022-12-13 02:48:14 +0600

    Add files via upload

---
+++ YOU KNOW ME.py
@@ -0,0 +1,133 @@
+print ("LET's See HOW YOU KNOW ABOUT MEHETAB HASAN")
+
+print ("")
+
+a=str (input ("WHAT IS MY FULL NAME?\n"))
+b="Mehetab Hasan Dipto"
+
+print ("")
+
+if (a==b):
+    print ("PAss korsen")
+else:
+    print("Tumi amare bal chino")
+
+print ("")
+
+c=str(input ("WHICH FOOD I LIKE MOST?\n"))
+b1="Biriyani"
+b2="Khichuri"
+
+print ("")
+
+if (c==b2):
+    print ("Yes AMar onek favorite")
+elif (c==b1):
+    print ("Yes ,I like it bUt I like kichuri more than it ")
+else :
+    print ("WRONG!!!!Amar basay dawat roilo amre chinna jaio")
+
+print ("")
+
+
+c1=str (input('Who is my BEst freind ?\n'))
+c2="Sazzad"
+
+print ("")
+
+if (c1==c2):
+    print ("YES TUMI Amare Mota moti chino")
+
+else :
+    print("AMr bisoy Dharona Kom")
+
+print ("")
+
+c3=str (input('AM I SINGLE ?'))
+c4="Yes"
+
+print ("")
+
+if (c3==c4):
+    print ("YES TUMI AMAR ONEK KASER")
+
+else :
+    print("TUMI KASER NA ETO")
+
+
+
+print ("")
+
+b11=str (input('HOW MANY FREINDS I HAVE WHO ALWAYS HELP ME?\n'))
+b22="Three"
+b33="Four"
+
+if (b11==b22):
+    print ("YEs TUmi EKebarei Kaser AKjn")
+
+
+elif (b11==b33):
+    print ("HA but Mainly three")
+
+
+else :
+    ("NAH TUMI EKHONO KASER KEO NA")
+
+
+
+print ("")
+
+
+c11=str (input("WHICH FOOD I HATE MOST?\n") )
+c22="Janina"
+if (c11==c22):
+    print ("AMi Nijeo JAnina Lol")
+
+else :
+    ("Hote pare")
+
+print ("")
+
+b111=str (input ("WHAT IS MY HOBBY?\n"))
+b222="Gaming"
+b333="Programming"
+
+print ("")
+
+if (b111==b222):
+    print ("YES YOU ARE RIGHT ")
+elif (b111==b333):
+    print ("Yes BUt gaming is besi moja")
+
+else:
+    print ("Amar bisoy Idea nai tomar")
+
+print ("")
+
+print ("LAST BUT NOT THE LEAST\n")
+print ("")
+j=str (input('HOW AM I?\n'))
+sii="Good"
+bii="Bad"
+if (j==sii):
+    print ("THANKS AND STAY SAFE")
+elif (j==bii):
+    print ("I Wanna know why")
+else:
+    print ("SUnaM naki durnam bujtasi na")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
